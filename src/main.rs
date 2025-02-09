476e94c1f696da79bd6f634a50666278b831e2de
Muyiwa <maoja37@gmail.com>
2025-02-09 18:30:49 +0100

    added comments

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,33 @@ fn main() {
         println!("Do you want to convert from farhenheit to celcius or from celsius to farhenheit");
         println!("To convert from celcius to farhenheit press C, To convert from farhenheit to celsius press F");
 
+        //create a variable to store the wether or not you want to convert celsius or fahrenheit
         let mut conversion_option = String::new();
+
+        //this is used for taking input
         io::stdin()
             .read_line(&mut conversion_option)
             .expect("Failed to read line");
 
+        //you have to convert to lower case in case the user types something
         let conversion_option = conversion_option.trim().to_lowercase();
 
+        // you have to add a as_str() function to this because by default "strings"
+        // here return as &str and not String and the variable was declared as a string
         match conversion_option.as_str() {
             "c" => {
                 println!("Enter the temperature in celsius");
 
+                //the variable was declared as a floating point for more precision and to avoid truncations
                 let temp: f64 = read_temperature();
                 println!(
                     "the converted temperature of {temp}°C to Fahrenheit is {}°F",
                     convert_to_fahrenheit(temp)
                 );
-                if !ask_to_continue(){
+                // this method returns true except when the user presses 'n' so in that case it returns
+                // false and this if statements negates it so it can be equal to true and it can enter the block
+
+                if !ask_to_continue() {
                     break;
                 }
             }
